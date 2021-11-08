9e5c74f88ade7eb98f28a7dbc3c0455c22a4e58e
Dudu244 <93841567+Dudu244@users.noreply.github.com>
2021-11-08 18:49:36 +0100

    Update covid22.lua

--- covid22.lua
+++ covid22.lua
@@ -1,76 +1,35 @@
-local a = {
-	DoubleTap = {ui.reference("RAGE", "OTHER", "Double tap")},
-    fakelag_enable = ui.reference("AA", "Fake lag", "Enabled"),
-    fakelag_type = ui.reference("AA", "Fake lag", "Amount"),
-    fakelag_limit = ui.reference("AA", "Fake lag", "Limit"),
-    fakelag_variance = ui.reference("AA", "Fake lag", "Variance"),
-    Fakeduck = ui.reference("RAGE", "OTHER", "Duck peek assist"),
-    pitch = ui.reference("AA", "Anti-aimbot angles", "Pitch"),
-    yaw_base = ui.reference("AA", "Anti-aimbot angles", "Yaw base"),
-    yaw = {ui.reference("AA", "Anti-aimbot angles", "Yaw")},
-    yaw_jitter = {ui.reference("AA", "Anti-aimbot angles", "Yaw jitter")},
-    body = {ui.reference("AA", "Anti-aimbot angles", "Body yaw")},
-    limit = ui.reference("AA", "Anti-aimbot angles", "Fake yaw limit"),
-    autodirection = {ui.reference("AA", "Anti-aimbot angles", "Freestanding")},
-    edge = ui.reference("AA", "Anti-aimbot angles", "Edge yaw"),
-    FreestandingBodyYaw = ui.reference("AA", "Anti-aimbot angles", "Freestanding body yaw"),
-    Enabled = ui.reference("AA", "Anti-aimbot angles", "Enabled"),
-    body_slider = ui.reference("AA", "Anti-aimbot angles", "Body yaw"),
-    slowmotion_type = ui.reference("AA", "Other", "Slow motion type"),
-    Onshot = {ui.reference("AA", "Other", "On shot anti-aim")},
-    DoubleTap = {ui.reference("RAGE", "OTHER", "Double tap")}
-}
-local b = {
-    main_toggle = ui.new_checkbox("AA", "Other", "[kvasibo] Flick"),
-    mAAEPeekFreestand = ui.new_hotkey("AA", "Other", "Enable"),
-    mAAEPeekInvert = ui.new_hotkey("AA", "Other", "Inverter"),
-    EPeekCombo = ui.new_combobox("AA", "Other", "Mode", "Flick")
-}
-local c = require "gamesense/surface"
-local d = c.draw_text
-local e = c.create_font("Tahoma", 12, 700, {0x200})
-local f = c.create_font("Tahoma", 15, 700, {0x200})
-local function g()
-    local h = ui.get(b.main_toggle)
-    ui.set_visible(b.mAAEPeekInvert, h)
-    ui.set_visible(b.mAAEPeekFreestand, h)
-    ui.set_visible(b.EPeekCombo, h)
-end
-client.set_event_callback("pre_render", g)
-local function i()
-    local j = globals.tickcount() % 19
-    local k, l = client.screen_size()
-    local m = {k / 2, l / 2}
-    local n = ui.get(a.yaw[2])
-    local o = {255 - (n + 212) / j * 2.29824561404, (n + 212) / j * 3.42105263158, (n + 212) / j * 0.22807017543}
-    local p = ui.get(b.mAAEPeekFreestand)
-    if p and ui.get(a.DoubleTap[2]) then
-        if ui.get(b.mAAEPeekInvert) then
-            d(m[1] - 48, m[2] + 13, o[1], o[2], o[3], 255, f, "<-")
-        end
-        if not ui.get(b.mAAEPeekInvert) then
-            d(m[1] + 41, m[2] + 13, o[1], o[2], o[3], 255, f, "->")
-        end
-        if not p then
-            ui.set(a.yaw[2], "0")
-        end
-        if j == 5 and ui.get(a.DoubleTap[2]) and p or j == 10 and ui.get(a.DoubleTap[2]) and p then
-            ui.set(a.yaw[1], "180")
-            if ui.get(b.mAAEPeekInvert) then
-                ui.set(a.yaw[2], "-90")
-            end
-            if not ui.get(b.mAAEPeekInvert) then
-                ui.set(a.yaw[2], "90")
-            end
-            client.exec("-use")
-        else
-            if ui.get(b.EPeekCombo) == "Flick" then
-                ui.set(a.yaw[2], "0")
-            elseif ui.get(b.EPeekCombo) == "Legit" then
-                ui.set(a.yaw[2], "180")
-            end
-        end
-    end
-end
-client.set_event_callback("paint", i)
+local enable = ui.new_checkbox("AA", "Other", "[beta] Kvasibo Resolver")
 
+local angles = { [0] = -58, [1] = 58, [2] = -48, [3] = 48, [4] = -40, [5] = 40, [6] = -15, [7] = 15, [8] = -30, [9] = 30, [10] = 0 } -- magic values
+local last_angle = 0
+local new_angle = 0
+
+local function resolve(player)
+    plist.set(player, "Correction active", false) -- disable default correction because i have a superiority complex
+    plist.set(player, "Force body yaw", true) -- enable the forcing of the body yaw
+    new_angle = angles[math.random(0, 10)]
+    client.color_log(0, 255, 204, "[Kvasibo Resolver] missed player: " .. entity.get_player_name(player) .. ", at angle: " .. last_angle .. ", bruteforced to: " .. new_angle)
+    plist.set(player, "Force body yaw value", new_angle) -- force yaw value to random
+    last_angle = new_angle
+end
+
+client.set_event_callback("aim_miss", function(info)
+    if not ui.get(enable) or info.reason ~= "?" then -- make sure we missed due to resolver :o
+        return
+    end
+    resolve(info.target) -- resolve that noob
+end)
+
+local chat = require "gamesense/chat"
+local hitgroup_names = {"generic", "head", "chest", "stomach", "left arm", "right arm", "left leg", "right leg", "neck", "?", "gear"}
+
+client.set_event_callback("player_hurt", function(e)
+	if e.userid ~= e.attacker and client.userid_to_entindex(e.attacker) == entity.get_local_player() then
+		local dmg, hp = e.dmg_health, e.health
+		local name = entity.get_player_name(client.userid_to_entindex(e.userid))
+		local hitgroup = hitgroup_names[e.hitgroup+1] or "gear"
+
+		chat.print("{darkred}Kvasibo {white}- {lightgreen}Hit {darkred}", name, "{lightgreen} in the {darkred}", hitgroup, "{lightgreen} for {darkred}", dmg, "{lightgreen} damage (", hp, " health remaining)")
+		
+	end
+end)
