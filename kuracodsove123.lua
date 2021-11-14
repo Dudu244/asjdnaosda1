ff9eb18c8b204aa9cc4a8d7ec5bfcbcf07bcfaed
Dudu244 <93841567+Dudu244@users.noreply.github.com>
2021-11-14 22:13:52 +0100

    Update kuracodsove123.lua

--- kuracodsove123.lua
+++ kuracodsove123.lua
@@ -22,6 +22,7 @@ local api = js.MyPersonaAPI
 local name = api.GetName()
 ----add player------------------------------
 if (playersteamid == Player1) then
+if (playersteamid == Player2) then
 --------------------------------------------
 whitelistsucess = true
 client.color_log(180, 238, 0, '       <---<[\0')
